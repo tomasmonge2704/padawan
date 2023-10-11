106cf6fe7315818765b91c5895206b8463add97c
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-10-11 00:41:01 -0300

    first commit

---
+++ src/lang/en.js
@@ -0,0 +1,46 @@
+export const textEn = {
+    home: "A new way of software development is born to be used in your company. We understand the reality of your company, which is why at PADAWAN we believe that the best way to work is to take the path towards their digital transformation togetherbased on our experience.",
+    ourWayTitle: "Our Way",
+    guidanceTitle: "Guidance",
+    guidance:
+      "We aim to guide our clients through their digital transformation journey.",
+    solutionsTitle: "Solutions",
+    solutions:
+      "We provide effective solutions to ensure the success of this process.",
+    supportTitle: "Support",
+    support: "We offer close support to guarantee a smooth transformation.",
+    leaderShipTitle: "Leadership",
+    leaderShip:
+      "We strive to become the leading technology consultancy, providing expert guidance to improve our clients' results.",
+    ourServicesTitle: "Our Services",
+    consultingTitle: "Consulting",
+    consulting:
+      "We evaluate your current situation, define a strategic roadmap, provide technical solutions and an implementation plan. We also provide training to ensure successful adoption.",
+    SoftwareDevelopmentTitle: "Software Development",
+    SoftwareDevelopment:
+      "We design and implement customized software solutions to enhance your operations.",
+    ongoingSuportTitle: "Ongoing Support",
+    ongoingSuport:
+      "We guarantee the optimal functioning of your systems with maintenance, upgrades and permanent assistance.",
+    ourMetodologyTitle: "Our Methodology",
+    colaborationTitle: "Collaboration",
+    colaboration:
+      "We work closely with your teams to understand your needs and adapt to your reality.",
+    customizationTitle: "Customization",
+    customization:
+      "We develop unique technology solutions that cater to each client's specific situation.",
+    comprehensiveSupportTitle: "Comprehensive Support",
+    comprehensiveSupport:
+      "We provide step by step guidance throughout the process to ensure success.",
+    knowladgeTransferTitle: "Knowledge Transfer",
+    knowladgeTransfer:
+      "We train your staff to optimize the use of our solutions.",
+    ourTeamTitle: "Our Team",
+    ourTeamSubtitle: "Network of Developers and consultants",
+    ourTeam:
+      "We work with a network of experts who join due to project demand. Do you want to work with us? Contact to hr@padawanway.com",
+    contactUsTitle: "Contact Us",
+    contactUsSubTitle: "Let's Transform Your Business Together",
+    desc: "Let us help you achieve your business goals throughdigital transformation. Contact to info@padawanway.com",
+    footerRightText: "About Us",
+  };
---
+++ src/lang/es.js
@@ -0,0 +1,46 @@
+export const textEs = {
+    home: "Nace una nueva forma de desarrollo de software para ser utilizado en su empresa. Entendemos la realidad de tu empresa, por eso en PADAWAN creemos que la mejor manera de trabajar es emprender juntos el camino hacia su transformación digital basándonos en nuestra experiencia.",
+    ourWayTitle: "Nuestra Forma",
+    guidanceTitle: "Orientación",
+    guidance:
+      "Nuestro objetivo es guiar a nuestros clientes a través de su viaje de transformación digital.",
+    solutionsTitle: "Soluciones",
+    solutions:
+      "Aportamos soluciones eficaces para garantizar el éxito de este proceso.",
+    supportTitle: "Soporte",
+    support: "Ofrecemos un apoyo cercano para garantizar una transformación fluida.",
+    leaderShipTitle: "Liderazgo",
+    leaderShip:
+      "Nos esforzamos por convertirnos en la consultoría tecnológica líder, brindando asesoramiento experto para mejorar los resultados de nuestros clientes.",
+    ourServicesTitle: "Nuestros Servicios",
+    consultingTitle: "Consultoría",
+    consulting:
+      "Evaluamos su situación actual, definimos una hoja de ruta estratégica, brindamos soluciones técnicas y un plan de implementación. También brindamos capacitación para garantizar una adopción exitosa.",
+    SoftwareDevelopmentTitle: "Desarrollo de Software",
+    SoftwareDevelopment:
+      "Diseñamos e implementamos soluciones de software personalizadas para mejorar sus operaciones.",
+    ongoingSuportTitle: "Soporte Continuo",
+    ongoingSuport:
+      "Garantizamos el óptimo funcionamiento de sus sistemas con mantenimiento, actualizaciones y asistencia permanente.",
+    ourMetodologyTitle: "Nuestra Metodología",
+    colaborationTitle: "Colaboración",
+    colaboration:
+      "Trabajamos estrechamente con tus equipos para entender tus necesidades y adaptarnos a tu realidad.",
+    customizationTitle: "Personalización",
+    customization:
+      "Desarrollamos soluciones tecnológicas únicas que se adaptan a la situación específica de cada cliente.",
+    comprehensiveSupportTitle: "Soporte Integral",
+    comprehensiveSupport:
+      "Proporcionamos orientación paso a paso durante todo el proceso para garantizar el éxito.",
+    knowladgeTransferTitle: "Transferencia de Conocimientos",
+    knowladgeTransfer:
+      "Capacitamos a su personal para optimizar el uso de nuestras soluciones.",
+    ourTeamTitle: "Nuestro Equipo",
+    ourTeamSubtitle: "Red de Desarrolladores y Consultores",
+    ourTeam:
+      "Trabajamos con una red de expertos que se incorporan por demanda del proyecto. ¿Quieres trabajar con nosotros? Contacta a hr@padawanway.com",
+    contactUsTitle: "Contáctanos",
+    contactUsSubTitle: "Transformemos Tu Negocio Juntos",
+    desc: "Permítenos ayudarte a alcanzar tus objetivos empresariales a través de la transformación digital. Contáctanos en info@padawanway.com",
+    footerRightText: "Acerca de Nosotros",
+  };
